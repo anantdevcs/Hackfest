aa2ecbd7bdab01b3130677e401a275a8420c375a
anantdevcs <anantdeviitism@gmail.com>
2020-03-07 13:44:10 +0530

    commit

--- app.py
+++ app.py
@@ -1,9 +1,27 @@
 from flask import Flask, escape, request, render_template
-
+from flask import  request,render_template,redirect,url_for,jsonify
+import os
 app = Flask(__name__)
+app.config["IMAGE_UPLOADS"] = "/static/images"
 
 @app.route('/')
 def hello():
     
     return render_template('index.html')
-app.run()
+
+@app.route('/upload-image',methods=['GET','POST'])
+
+def upload():
+	if request.method == "POST":
+		if request.files:
+
+		            image = request.files["file"]
+
+		            image.save(image.filename)
+
+		            print("Image saved")
+
+		            return redirect(request.url)
+	else:
+		return render_template('index.html')	            
+app.run(debug=True)
